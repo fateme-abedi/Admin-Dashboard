dd7e982fa4eaacb868a3d1a7cb1adb0348a5ed85
fateme <abedifateme1378@gmail.com>
2022-08-25 10:47:14 +0430

    Data of cards updated

--- src/components/cards/CardsData.js
+++ src/components/cards/CardsData.js
@@ -55,25 +55,5 @@ export const cardsData = [
     },
   ];
   export default cardsData;
-  // Recent Update Card Data
-  // export const UpdatesData = [
-  //   {
-  //     img: img1,
-  //     name: "Andrew Thomas",
-  //     noti: "has ordered Apple smart watch 2500mh battery.",
-  //     time: "25 seconds ago",
-  //   },
-  //   {
-  //     img: img2,
-  //     name: "James Bond",
-  //     noti: "has received Samsung gadget for charging battery.",
-  //     time: "30 minutes ago",
-  //   },
-  //   {
-  //     img: img3,
-  //     name: "Iron Man",
-  //     noti: "has ordered Apple smart watch, samsung Gear 2500mh battery.",
-  //     time: "2 hours ago",
-  //   },
-  // ];
+  
   
